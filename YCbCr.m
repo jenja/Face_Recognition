928ca3794488fbb866199a435a4836d2c73049ba
christoffer engelbrektsson <chren574@student.liu.se>
2015-11-10 10:44:05 +0100

    Experimented with YCbCr

--- YCbCr.m
+++ YCbCr.m
@@ -1,28 +1,33 @@
 function out = YCbCr( im )
 %UNTITLED3 Summary of this function goes here
-%   Detailed explanation goes here
+% 
 
+% Whitebalande the picture
 im = whitebalance(im);
 
+% Convert pixelvalues to doubles
 im = im2double(im);
-YCbCr = rgb2ycbcr(im);
 
+% Change the colorspace to YCbCr
+YCbCr = rgb2ycbcr(im);
 Y = YCbCr(:,:,1);
 Cb = YCbCr(:,:,2);
 Cr = YCbCr(:,:,3);
 
-Cr(  (Cr > (105/255)) & (Cr < (135/255)) ) = 0;
-
-%Cb(Cb < (105/255) ) = 0;
-%Cb(Cb > (135/255) ) = 0;
+% Set irrelevent pixels to black
+%YCbCr(  (Cr > (105/255)) & (Cr < (135/255))) = 0;
+Cr(  (Cr > (105/255)) & (Cr < (135/255))) = 0;
+%Cr(  (Cr > (140/255)) & (Cr < (165/255)) ) = 0;
+%YCbCr(  (Cr > (140/255)) & (Cr < (165/255)) & (Cb > (105/255)) & (Cb < (135/255)) ) = 0;
 
 %Otsu's method
-level = graythresh(Cr)
+level = graythresh(YCbCr)
 
-Cr = im2bw(Cr, level);
-%imshow(Cr)
-% figure
-% imshow(Cb)
+Cri = im2bw(Cr , level);
+%Cri = im2bw(YCbCr , 0.4);
+
+figure
+imshow(Cri)
 
 % se = strel('disk', 3);
 % se2 = strel('disk', 9);
@@ -32,19 +37,26 @@ se = strel('disk', 3);
 se2 = strel('disk', 20);
 se3 = strel('disk', 12);
 
-MorphFace  = imerode(imdilate(imerode(Cr, se), se2), se3);
+% erosion and dialation
+MorphFace  = imerode(imdilate(imerode(Cri, se), se2), se3);
+
+% Fill all leftover holes
 MorphFace = imfill(MorphFace, 'holes');
+
 figure, imshow(MorphFace)
 
+% Convert to RGB and seperate the channels
 RGB = ycbcr2rgb(YCbCr);
 R = RGB(:,:,1);
 G = RGB(:,:,2);
 B = RGB(:,:,3);
 
+% Segmentation
 R(MorphFace ~= 1) = 0;
 G(MorphFace ~= 1) = 0;
 B(MorphFace ~= 1) = 0;
 
+% Return RGB-chanels to one picture
 Result = cat(3, R, G, B);
 
 figure
--- test_all.m
+++ test_all.m
@@ -1,7 +1,7 @@
-%im = imread('DB0/db0_3.jpg');
+im = imread('DB0/db0_3.jpg');
 %im = imread('DB1/db1_01.jpg');
 %im = imread('DB1/db1_08.jpg');
-im = imread('DB1/db1_16.jpg');
+%im = imread('DB1/db1_16.jpg');
 
 YCbCr(im)
 
