33437ea094c04a0f8cfe5332b5160d50b96726b2
ronjagrosz <rongr946@student.liu.se>
2015-11-12 14:27:30 +0100

    eigen face, not sure if correct

--- eigenface.m
+++ eigenface.m
@@ -1,4 +1,10 @@
 %Change this to a function later on...
+%Calculates the eigenfaces of the images in the DB
+%Project a query image onto the eigenfaces
+%Compare have much it differs
+%Should return the eigenface with 
+%the smallest euklidian distance, but not working yet
+close all;
 
 M = 3;
 p = 303*241;
@@ -24,9 +30,28 @@ imshow(uint8(reshape(round(avgFace), 303, 241)));
 
 %Calculate have much each image differs from the average
 for i = 1:M
-   F(:,i) = (F(:,1) - avgFace)/sqrt(2); 
+   F(:,i) = (imgDB(:,i) - avgFace)/sqrt(2); 
 end
 
+% figure 
+% imshow(uint8(reshape(round(F(:,1)), 303, 241)));
+% figure 
+% imshow(uint8(reshape(round(F(:,2)), 303, 241)));
+% figure 
+% imshow(uint8(reshape(round(F(:,3)), 303, 241)));
 
+[u,~] = eig(F'*F);
 
-%
+d = (u*F')';
+
+for i = 1:M 
+figure
+colormap gray
+imagesc(reshape(d(:,i),303,241)); 
+end
+
+for i = 1:M
+    w(:,i) = (imgDB(:,3) - avgFace)./d(:,i);
+    q(i) = nansum(w(:,i));
+end
+
