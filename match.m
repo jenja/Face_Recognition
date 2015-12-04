468a221df746be226895494168f89f13d8d468d5
ronjagrosz <rongr946@student.liu.se>
2015-12-04 21:05:06 +0100

    LPQ

--- match.m
+++ match.m
@@ -1,18 +1,34 @@
-function id = match(im)
-% MATCH
-%   Match image with the database and return an integer.
-%   id = 0 if no match
-%   id = index if match
-
+% function id = match(im)
+%match image with the database
+%id = 0 if no match
+%id = index if match
+close all;
 %Load training set
 load weights.mat
 load eigenvectors.mat
 
+%REMOVE WHEN EVERYTHING WORKS
+im = imread('ps/db1_14.jpg');
+
+%im should be in grey scale, 
+%Later on im will be preprocessed
+%and this step is no longer needed
+im = double(rgb2gray(im));
+
 wImg = ((reshape(im, [], 1))'*U)';
+
+v = zeros (303*241,1);
+for i = 1:14
+    %reconstruct an image
+    figure
+    v = v + w(i,14)'*U(:,i);
+    imshow(reshape((v),303,241),[]);
+end
+
 
 index = 0;
 winner = 99999;
-for i = 1:16
+for i = 1:14
    near = wImg - w(:,i);
    near = sqrt(near'*near);
    if near < winner 
@@ -21,12 +37,12 @@ for i = 1:16
    end
 end
 
-%Threshold may not be correct, but works fine for now
-threshold = 1000;
+%choose a better threshold?
+threshold = 7000;
 if winner > threshold
     index = 0;
 end
 
 id = index;
 
-end
+% end
