85472d09522b2c80efb7eef1f65e665d28702457
Chistoffer Engelbrektsson <chren574@student.liu.se>
2016-08-31 11:04:20 +0200

    Added comments

--- FindMouth.m
+++ FindMouth.m
@@ -9,13 +9,17 @@
 function Mouth = FindMouth( mouthMap )
 % Find the coordinate of the mouth in the image 
 
+% Create a black & white image with a treshold
 bwmouth = im2bw(mouthMap, 0.3);
 
+% How many rows the image have
 [row, ~] = size(mouthMap);
 
+% Remove noice and unnecessary data around the center where the mouth likly is.
 bwmouth(1:(floor(2*row/3 - 50)), :) = 0;
 bwmouth(floor(3*row/4 + 50):row, :) = 0;
 
+% Morphological structuring element of two disks with different radii
 se = strel('disk', 1);
 se2 = strel('disk', 3);
 
@@ -28,11 +32,11 @@ bwmouth = imdilate(bwmouth, se2);
 
 title('Image with Circles')
 
+% Sort the data in the image and create a table with it
 stats = regionprops('table',bwmouth,'Centroid', 'MajorAxisLength','MinorAxisLength');
-
 table = sortrows(stats, 3, 'descend');
 
-
+% The data with the heighest value is most likly to be the mouth 
 Mouth = round(table2array(table(1,1)))
 
 end
